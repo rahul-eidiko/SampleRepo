552b283205da8c161a37b96994e17889e9df073c
rahul <macharahul@outlook.com>
2018-06-07 18:07:01 +0530

    Updating DisplayString with new line & added readme file

--- DisplayString.java
+++ DisplayString.java
@@ -1,9 +1,10 @@
 public class DisplayString{
 
-public static void main(String args[]){
+ public static void main(String[] args){
 
-System.out.println("*** Display String ****");
+  System.out.println("*** Display String ****");
 
-}
+  System.out.println("*** Adding this line after commit");
+ }
 
 }
